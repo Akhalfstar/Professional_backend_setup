fba71b0d441e578084db7def691112be223c8c04
Aman katiyar <amankatiyar783597@gmail.com>
2024-01-24 23:46:49 +0530

    user model added correction wrong file

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,5 +1,51 @@
 import mongoose from 'mongoose'
 
-const userScheema = new mongoose.Schema({} , {timestamps:true})
+const userScheema = new mongoose.Schema({
+    username :{
+        type : String,
+        unique : true,
+        required : true,
+        lowecase : true,
+        trim : true,
+        index : true
+    },
+    email :{
+        type : String,
+        unique : true,
+        required : true,
+        lowecase : true,
+        trim : true
+    },
+    fullname :{
+        type : String,
+        required : true,
+        trim : true,
+        index : true
+    },
+    avtar :{
+        type : String,
+        unique : true,
+        required : true,
+    },
+    username :{
+        type : String,
+    },
+    watchHistory :[
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Video"
+        }  
+    ],
+    password :{
+        type : String,
+        required : [true , "password is required"]
+    },
+    refreshToken :{
+        type : String,
+        required : true
+    },
+
+
+} , {timestamps:true})
 
 export const User = mongoose.model("user" , userScheema)
--- src/model/video.model.js
+++ src/model/video.model.js
@@ -1,51 +1,7 @@
 import mongoose from 'mongoose'
 
 const VideoScheema = new mongoose.Schema({
-    username :{
-        type : String,
-        unique : true,
-        required : true,
-        lowecase : true,
-        trim : true,
-        index : true
-    },
-    email :{
-        type : String,
-        unique : true,
-        required : true,
-        lowecase : true,
-        trim : true
-    },
-    fullname :{
-        type : String,
-        required : true,
-        trim : true,
-        index : true
-    },
-    avtar :{
-        type : String,
-        unique : true,
-        required : true,
-    },
-    username :{
-        type : String,
-    },
-    watchHistory :[
-        {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "Video"
-        }  
-    ],
-    password :{
-        type : String,
-        required : [true , "password is required"]
-    },
-    refreshToken :{
-        type : String,
-        required : true
-    }
-
-
+    
 
 
 } , {timestamps:true})
