23784a3cea61403c9c54da9cb08ff6470976e98f
Aman katiyar <amankatiyar783597@gmail.com>
2024-01-24 23:53:04 +0530

    video model added

--- src/model/video.model.js
+++ src/model/video.model.js
@@ -1,7 +1,38 @@
 import mongoose from 'mongoose'
 
 const VideoScheema = new mongoose.Schema({
-    
+    videoFile :{
+        type : String,
+        required : true,
+    },
+    thumbnail :{
+        type : String,
+        required : true,
+    },
+    tital :{
+        type : String,
+        required : true,
+    },
+    description :{
+        type : String,
+        required : true,
+    },
+    duration :{
+        type : String, // cloudnary
+        required : true,
+    },
+    views :{
+        type : Number,
+        default : 0,
+    },
+    isPublished :{
+        type : Boolean,
+        default : true,
+    },
+    owner :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    }
 
 
 } , {timestamps:true})
