6712b7ed5dd34a9b726b9224c2499f8d95746d42
Aman katiyar <amankatiyar783597@gmail.com>
2024-01-24 11:26:30 +0530

    utilities set

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import 'dotenv/config'
 
 import dbConnect from "./dbs/index.js";
+import express from 'express'
+
+const app = express()
+
+
 
 dbConnect()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log("server is running on port : " , process.env.PORT)
+    })
+})
+.catch((error)=>{
+    console.log("Error after connecting DB : " , error)
+})
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,21 @@
+import { json } from "express"
+
+const asynchandler = (fn) =>{
+    (req, res, next) => {
+        return Promise.resolve(fn(req , res , next)).catch((error)=>next(error))
+    }
+}
+
+const ash2 = (fn) => async (req , res , next)=> {
+    try {
+        
+    } catch (error) {
+        res.status(error.code || 500)
+        .json({
+            success :false,
+            message : error.message
+        })
+    }
+}
+
+export {asynchandler , ash2}
