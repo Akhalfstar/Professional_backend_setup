945af7c71a44cdefa12abd49fd0e2187e96ad547
Aman katiyar <amankatiyar783597@gmail.com>
2024-01-25 10:06:27 +0530

    user model methords and passworn encry

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -51,6 +51,38 @@ const userScheema = new mongoose.Schema({
 
 } , {timestamps:true})
 
+userScheema.pre("save" , async function (next){
+    if(!this.isModified("password")) next()
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+})
 
+userScheema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password , this.password)
+}
+
+userScheema.methods.genrateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userScheema.methods.genrateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("user" , userScheema)
